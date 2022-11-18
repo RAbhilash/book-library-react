0ce9b4c67460eab2f28a7962d68eb4273bde4817
RAbhilash <gitabhi26@gmail.com>
2022-11-18 17:05:01 +0530

    landing page heading reveal animation

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -3,8 +3,8 @@ import "./landing.css";
 function landing() {
   return (
     <div id="landing">
-        <h2>LECTORI SALUTEM</h2>
-        <button></button>
+        <h2>LECTORI SALUTEM <div id="fadeInOverlay"></div></h2> 
+        <button>MY BOOKS</button>
     </div>
   )
 }
